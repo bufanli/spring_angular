f16b3e1d243ef483a2fea462bd92cc76b330a5ac
fanli.bu <xiaojie415@163.com>
2018-12-12 22:39:20 +0800

    admin login function is implementing.

--- angular/src/app/login/components/admin-login/admin-login.component.ts
+++ angular/src/app/login/components/admin-login/admin-login.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminLoginComponent implements OnInit {
 
+  private adminName: string;
+  private password: string;
   constructor() { }
 
   ngOnInit() {
