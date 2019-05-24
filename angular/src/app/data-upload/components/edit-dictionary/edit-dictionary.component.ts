de0a78cc235f47c6ddbb549f22ce2c9d8590c6a7
fanli.bu <xiaojie415@163.com>
2019-05-24 22:51:38 +0800

    edit dictionary test code commit

--- angular/src/app/data-upload/components/edit-dictionary/edit-dictionary.component.ts
+++ angular/src/app/data-upload/components/edit-dictionary/edit-dictionary.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewChecked } from '@angular/core';
 
 @Component({
   selector: 'app-edit-dictionary',
   templateUrl: './edit-dictionary.component.html',
   styleUrls: ['./edit-dictionary.component.css']
 })
-export class EditDictionaryComponent implements OnInit {
-
+export class EditDictionaryComponent implements OnInit, AfterViewChecked {
+  public originalColumns: string[] = ['日期'];
+  public synonyms: any = [{
+    originalColumn: '年月日',
+  }];
   constructor() { }
 
   ngOnInit() {
+  }
+  ngAfterViewChecked() {
+    $('#table').bootstrapTable({
+      data: this.synonyms,
+      height: $(window).height() * 0.6,
+    });
   }
 
 }
